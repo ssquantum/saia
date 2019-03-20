a49c76f4e0b44a62fa24254482eeba542b593b99
SPENCE <qgtx64@durham.ac.uk>
2019-03-20 18:51:34 +0000

    updating imageHandler.py

--- imageHandler.py
+++ imageHandler.py
@@ -12,6 +12,7 @@ Assuming that image files are ASCII.
 import os
 import sys
 import numpy as np
+import time
 
 
 def est_param(x, y):
@@ -59,22 +60,34 @@ class image_handler:
         self.thresh = 1                 # initial threshold for atom detection
         self.atom = np.zeros(self.n)    # deduce presence of an atom by comparison with threshold
         # file label list length < integrated counts so that no data is lost when extra spaces have to be appended
-        self.files = np.array([None]*(self.n-10)) # labels of files. 
+        self.files = np.array([None]*(self.n)) # labels of files. 
         self.im_num = 0                 # number of images processed
         self.im_vals = np.array([])     # the data from the last image is accessible to an image_handler instance
         self.bin_array = []             # if bins for the histogram are supplied, plotting can be faster
         
     def set_pic_size(self, im_name):
         """Set the pic size by looking at the number of columns in a file"""
-        im_vals = np.genfromtxt(im_name, delimeter=self.delim)
-        self.pic_size = int(np.size(im_vals[0]) - 1) # the first column of ASCII image is row #
+        im_vals = np.genfromtxt(im_name, delimiter=self.delim)
+        self.pic_size = int(np.size(im_vals[0]) - 1) # the first column of ASCII image is row number
+
+    def reset_arrays(self):
+        """Reset all of the histogram array data to zero"""
+        self.files = np.array([None]*(self.n)) # labels of files. 
+        self.counts = np.zeros(self.n)  # integrated counts from atom
+        self.mean_count = np.zeros(self.n) # list of mean counts in image - estimates background 
+        self.std_count = np.zeros(self.n)  # list of standard deviation of counts in image
+        self.xc_list = np.zeros(self.n) # horizontal positions of max pixel
+        self.yc_list = np.zeros(self.n) # vertical positions of max pixel
+        self.atom = np.zeros(self.n)    # deduce presence of an atom by comparison with threshold
+        self.im_num = 0                 # number of images processed
+        
         
     def load_full_im(self, im_name):
         """return an array with the values of the image"""
         #np.array(Image.open(im_name)) # for bmp images
-        #np.genfromtxt(im_name, delimiter=self.delim)[:,1:] # first column gives column number
+        # return np.genfromtxt(im_name, delimiter=self.delim)#[:,1:] # first column gives column number
         return np.loadtxt(im_name, delimiter=self.delim,
-                              usecols=range(1,self.pic_size+1))
+                              usecols=range(1,self.pic_size-1))
         
     def process(self, im_name):
         """Get the data from an image """
@@ -82,36 +95,62 @@ class image_handler:
             self.add_count(im_name)
             
         except IndexError: # this is a bad exception - the error might be from a bad ROI rather than reaching the end of the arrays
-            self.counts = np.append(self.counts, np.zeros(self.n))
-            self.mean_count = np.append(self.counts, np.zeros(self.n)) 
-            self.std_count = np.append(self.counts, np.zeros(self.n))
-            self.xc_list = np.append(self.xc_list, np.zeros(self.n))
-            self.yc_list = np.append(self.yc_list, np.zeros(self.n))
-            self.atom = np.append(self.atom, np.zeros(self.n))
-            self.files = np.append(self.files, np.array([None]*self.n))
-            
+            # filled the array of size n so add more elements
+            if self.im_num % (self.n - 10) == 0 and self.im_num > self.n / 2:
+                self.counts = np.append(self.counts, np.zeros(self.n))
+                self.mean_count = np.append(self.counts, np.zeros(self.n)) 
+                self.std_count = np.append(self.counts, np.zeros(self.n))
+                self.xc_list = np.append(self.xc_list, np.zeros(self.n))
+                self.yc_list = np.append(self.yc_list, np.zeros(self.n))
+                self.atom = np.append(self.atom, np.zeros(self.n))
+                self.files = np.append(self.files, np.array([None]*self.n))
+                
             self.add_count(im_name)
+
+    def add_int_count(self, im_name):
+        """Fill in the next index of the counts by summing over the ROI region and then 
+        getting a counts/pixel. 
+        Fill in the next index of the file, xc, yc, mean, std arrays."""
+        self.im_vals = np.genfromtxt(im_name, delimiter=self.delim)[self.yc-self.roi_size//2:
+            self.yc+self.roi_size//2, self.xc-self.roi_size//2:self.xc+self.roi_size//2]
+        # mean counts in the image (which should already be an ROI)
+        self.counts[self.im_num] = np.sum(self.im_vals) / np.size(self.im_vals)
+        
+        # naming convention: [Species]_[date]_[Dexter file #]
+        self.files[self.im_num] = im_name.split("_")[-1].split(".")[0]
+        
+        # find the position of the largest pixel
+        # note that the statistics of the background are undermined by always taking the max.
+        try:
+            self.xc_list[self.im_num], self.yc_list[self.im_num] = np.where(self.im_vals == np.max(self.im_vals))
+        except ValueError: # same maximum value found in more that one position
+            xcs, ycs = np.where(self.im_vals == np.max(self.im_vals))
+            self.xc_list[self.im_num], self.yc_list[self.im_num] = xcs[0], ycs[0]
+    
+        # background statistics: mean count and standard deviation across image
+        self.mean_count[self.im_num] = np.mean(self.im_vals)
+        self.std_count[self.im_num] = np.std(self.im_vals, ddof=1)
+        
+        self.im_num += 1
             
     def add_count(self, im_name):
-        """Fill the next index of the counts and files arrays"""
+        """Fill the next index of the counts with the max count in a pixel.
+        Fill in the next index of the file, xc, yc, mean, std arrays."""
         # could speed up by removing this if statement by having two different functions:
         # then the bool toggle to use the ROI changes which function is used.
         if self.roi_size > 0:
-            self.im_vals = np.genfromtxt(im_name, delimiter=self.delim)[self.yc-self.roi_size//2:
+            self.im_vals = self.load_full_im(im_name)[self.yc-self.roi_size//2:
             self.yc+self.roi_size//2, self.xc-self.roi_size//2:self.xc+self.roi_size//2]  # array of pixel values in ROI
         else:
             # note that ASCII file formats are variable... might have the last column is empty, comes out as NaN: [:,:-1]
             # might fail if trying to go really fast because the file hasn't been filled with data yet
-            self.im_vals = self.load_full_im(im_name)
-            if self.im_vals.size:
-                pass # checking that the array isn't empty
+            if os.stat(im_name).st_size: # check size of file in bytes (0 if unwritten) - this check only takes 0.2ms
+                self.im_vals = self.load_full_im(im_name)
             else:
                 print("File was empty, waiting 0.01s and trying again")
                 time.sleep(0.01)
                 self.im_vals = self.load_full_im(im_name)
         
-        # sum the counts in the image (which should already be an ROI)
-        # self.counts[self.im_num] = np.sum(self.im_vals)
         # take the max count in the image (undermines the statistics of the background)
         self.counts[self.im_num] = np.max(self.im_vals)
         
@@ -189,14 +228,17 @@ class image_handler:
         """Load back in the counts data from a stored csv file, leavning space
         in the arrays to add new data as well"""
         data = np.genfromtxt(file_name, delimiter=',', dtype=str)
+        fd = data[1:,1:].astype(float) # the numerical data
+
         # the first row is the header
-        self.files = np.concatenate(data[1:,0], np.array([None]*self.n))
-        self.counts = np.concatenate(data[1:,1], np.zeros(self.n))
-        self.atom = np.concatenate(data[1:,2], np.zeros(self.n))
-        self.xc_list = np.concatenate(data[1:,3], np.zeros(self.n))
-        self.yc_list = np.concatenate(data[1:,4], np.zeros(self.n))
-        self.mean_count = np.concatenate(data[1:,5], np.zeros(self.n)) 
-        self.std_count = np.concatenate(data[1:,6], np.zeros(self.n))
+        self.files = np.concatenate((self.files[:self.im_num], data[1:,0], np.array([None]*self.n)))
+        self.counts = np.concatenate((self.counts[:self.im_num], fd[:,0], np.zeros(self.n)))
+        self.atom = np.concatenate((self.atom[:self.im_num], fd[:,1], np.zeros(self.n)))
+        self.xc_list = np.concatenate((self.xc_list[:self.im_num], fd[:,2], np.zeros(self.n)))
+        self.yc_list = np.concatenate((self.yc_list[:self.im_num], fd[:,3], np.zeros(self.n)))
+        self.mean_count = np.concatenate((self.mean_count[:self.im_num], fd[:,4], np.zeros(self.n)))
+        self.std_count = np.concatenate((self.std_count[:self.im_num], fd[:,5], np.zeros(self.n)))
+        self.im_num += np.size(data[1:,0]) # now we have filled this many extra columns.
 
         
     def save_state(self, save_file_name):
@@ -214,4 +256,3 @@ class image_handler:
         np.savetxt(save_file_name, out_arr, fmt='%s', delimiter=',',
                 header='File, Counts, Atom Detected (threshold=%s), X-pos (pix), Y-pos (pix), Mean Count, s.d.'
                 %int(self.thresh))
-            
